b07a679c28e72540409088859941f0d5a8cd4329
ehhaque <ehaque@gmail.com>
2014-07-11 18:10:36 -0700

    search working2

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
   	 @search = Room.search(params[:q])
-     @rooms = @search.result()
+     #@rooms = @search.result()
+     @rooms = @search.result.includes(:hotel).page(params[:page])
 
     
     #@rooms = Room.paginate(page: params[:page])
